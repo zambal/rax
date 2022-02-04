d13884bd14b50257565573b2ee028a7087a6e0f6
Vincent Siliakus <vincent@usendes.com>
2022-02-04 14:47:12 +0100

    add scheduler state machine

---
+++ lib/rax/scheduler.ex
@@ -0,0 +1,164 @@
+defmodule Rax.Scheduler do
+  require Logger
+
+  # API
+
+  @type opts :: [opt]
+  @type opt :: {:interval, non_neg_integer()} |
+    {:type, :repeat | :once}  |
+    {:exclusive, boolean()}
+
+  @type func :: (-> any())
+
+  @type name :: atom()
+
+  @default_opts [
+    interval: 1000,
+    type: :repeat,
+    exclusive: true
+  ]
+
+  @spec set(Rax.Cluster.name(), name(), func(), opts()) :: :ok
+  def set(cluster, name, fun, opts \\ []) when is_function(fun, 0) do
+    case init_opts(opts) do
+      {:ok, opts} ->
+        Rax.call(cluster, {:set_timer, name, fun, opts})
+
+      :error ->
+        raise ArgumentError, message: "invalid opts: #{inspect opts}"
+    end
+  end
+
+  @spec remove(Rax.Cluster.name(), atom()) :: :ok | nil
+  def remove(cluster, name) do
+    Rax.call(cluster, {:remove, name})
+  end
+
+  @spec remove_all(Rax.Cluster.name()) :: :ok
+  def remove_all(cluster) do
+    Rax.call(cluster, :remove_all)
+  end
+
+  @spec list(Rax.Cluster.name()) :: Keyword.t()
+  def list(cluster) do
+    Rax.query(cluster, fn state ->
+      for {name, { _fun, opts}} <- state do
+        {name, opts}
+      end
+    end)
+  end
+
+  # State machine
+
+  use Rax.Machine
+  @doc false
+  def init(_config) do
+    %{}
+  end
+
+  @doc false
+  def apply(_meta, {:set_timer, name, fun, opts}, state) do
+    state = Map.put(state, name, {fun, opts})
+    interval = Keyword.fetch!(opts, :interval)
+    {state, :ok, [{:timer, name, interval}]}
+  end
+
+  def apply(_meta, {:remove, name}, state) do
+    if Map.has_key?(state, name) do
+      {Map.delete(state, name), :ok, [{:timer, name, :infinity}]}
+    else
+      {state, nil}
+    end
+  end
+
+  def apply(_meta, :remove_all, state) do
+    effects =
+      for {name, _} <- state do
+        {:timer, name, :infinity}
+      end
+
+    {%{}, :ok, effects}
+  end
+
+  def apply(_meta, {:timeout, name}, state) do
+    case Map.fetch(state, name) do
+      {:ok, {fun, opts}} ->
+        case handle_fun(fun, opts) do
+          {:cont, effect} ->
+            {state, effects} = handle_state(state, name, opts)
+            {state, :ok, [effect | effects]}
+
+          resp ->
+            {state, effects} = handle_state(state, name, opts)
+            {state, resp, effects}
+        end
+
+      :error ->
+        {state, nil}
+    end
+  end
+
+  def apply(meta, cmd, state) do
+    super(meta, cmd, state)
+  end
+
+  def state_enter(:leader, state) do
+    for {name, {_fun, opts}} <- state do
+      interval = Keyword.fetch!(opts, :interval)
+      {:timer, name, interval}
+    end
+  end
+
+  def state_enter(_, _state) do
+    []
+  end
+
+  @doc false
+  def apply_fun(fun) do
+    spawn(fun)
+  end
+
+  defp handle_fun(fun, opts) do
+    f =
+      fn ->
+        try do
+          fun.()
+          :ok
+        catch
+          type, reason ->
+            Logger.error("Rax Scheduler timer fun #{inspect type} with reason: #{inspect reason}\n#{inspect __STACKTRACE__}")
+            :error
+        end
+      end
+
+    if opts[:exclusive] do
+      {:cont, {:mod_call, Rax.Scheduler, :apply_fun, [f]}}
+    else
+      f.()
+    end
+  end
+
+  defp handle_state(state, name, opts) do
+    case Keyword.fetch!(opts, :type) do
+      :once ->
+        {Map.delete(state, name), []}
+
+      :repeat ->
+        interval = Keyword.fetch!(opts, :interval)
+        {state, [{:timer, name, interval}]}
+    end
+  end
+
+  defp init_opts(opts) do
+    opts = Keyword.merge(@default_opts, opts)
+    with {:ok, n} when (is_integer(n) and n >= 0) or n == :infinity <- Keyword.fetch(opts, :interval),
+        {:ok, type} when type in [:repeat, :once] <- Keyword.fetch(opts, :type),
+        exclusive when exclusive in [true, false] <- Keyword.get(opts, :exclusive, false)
+    do
+      {:ok, opts}
+    else
+      _ ->
+        :error
+    end
+  end
+end
