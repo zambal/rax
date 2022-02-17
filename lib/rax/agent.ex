1a4224baddf74ee0d7366a554cd2ed010528581b
Vincent Siliakus <vincent@usendes.com>
2022-02-17 10:54:24 +0100

    redesign Rax.Agent to make it crash safe

--- lib/rax/agent.ex
+++ lib/rax/agent.ex
@@ -16,7 +16,7 @@ defmodule Rax.Agent do
     |> handle_result()
   end
 
-  def get(cluster, agent, module, fun, args) do
+  def get(cluster, agent, module, fun, args \\ []) do
     Rax.query(cluster, &call(agent, fn s -> Kernel.apply(module, fun, [s | args]) end, &1.agents))
     |> handle_result()
   end
@@ -26,7 +26,7 @@ defmodule Rax.Agent do
     |> handle_result()
   end
 
-  def get_and_update(cluster, agent, module, fun, args) do
+  def get_and_update(cluster, agent, module, fun, args \\ []) do
     Rax.call(cluster, {:get_and_update, agent, module, fun, args})
     |> handle_result()
   end
@@ -36,8 +36,8 @@ defmodule Rax.Agent do
     |> handle_result()
   end
 
-  def update(cluster, agent, module, fun, args) do
-    Rax.call(cluster, {:update, agent, fn s -> Kernel.apply(module, fun, [s | args]) end})
+  def update(cluster, agent, module, fun, args \\ []) do
+    Rax.call(cluster, {:update, agent, &Kernel.apply(module, fun, [&1 | args])})
     |> handle_result()
   end
 
@@ -45,15 +45,32 @@ defmodule Rax.Agent do
     Rax.cast(cluster, {:update, agent, fun})
   end
 
-  def cast(cluster, agent, module, fun, args) do
-    Rax.cast(cluster, {:update, agent, fn s -> Kernel.apply(module, fun, [s | args]) end})
+  def cast(cluster, agent, module, fun, args \\ []) do
+    Rax.cast(cluster, {:update, agent, &Kernel.apply(module, fun, [&1 | args])})
   end
 
   defp handle_result(:ok), do: :ok
+
   defp handle_result({:ok, value}), do: value
 
   defp handle_result({:error, :no_agent, agent}),
     do: raise(ArgumentError, message: "agent not found: #{inspect(agent)}")
+
+  defp handle_result({:error, :agent_not_new, agent}),
+    do: raise(ArgumentError, message: "agent already exists: #{inspect(agent)}")
+
+  defp handle_result({:error, :badret, ret}),
+    do: raise(ArgumentError, message: "bad return value from agent, expected {reply, state}, got: #{inspect(ret)}")
+
+  defp handle_result({:error, {:throw, reason, _stacktrace}}),
+    do: throw(reason)
+
+  defp handle_result({:error, {:error, e, stacktrace}}),
+    do: reraise(Exception.normalize(:error, e, stacktrace), stacktrace)
+
+  defp handle_result({:error, {:exit, reason, _stacktrace}}),
+    do: exit(reason)
+
 
   # State Machine
 
@@ -67,7 +84,7 @@ defmodule Rax.Agent do
         {:error, :agent_not_new, agent}
 
       _ ->
-        {:ok, fun.()}
+        handle_fun(fun)
     end
     |> handle_update_result(agent, state)
   end
@@ -82,7 +99,7 @@ defmodule Rax.Agent do
   end
 
   def apply(_meta, {:get_and_update, agent, mod, fun, args}, state) do
-    call(agent, fn s -> Kernel.apply(mod, fun, [s | args]) end, state.agents)
+    call(agent, &Kernel.apply(mod, fun, [&1 | args]), state.agents)
     |> handle_get_and_update_result(agent, state)
   end
 
@@ -92,7 +109,7 @@ defmodule Rax.Agent do
   end
 
   def apply(_meta, {:update, agent, mod, fun, args}, state) do
-    call(agent, fn s -> Kernel.apply(mod, fun, [s | args]) end, state.agents)
+    call(agent, &Kernel.apply(mod, fun, [&1 | args]), state.agents)
     |> handle_update_result(agent, state)
   end
 
@@ -102,7 +119,7 @@ defmodule Rax.Agent do
 
   defp call(agent, fun, state) do
     if agent_state = Map.get(state, agent) do
-      {:ok, fun.(agent_state)}
+      handle_fun(fun, agent_state)
     else
       {:error, :no_agent, agent}
     end
@@ -127,5 +144,23 @@ defmodule Rax.Agent do
 
   defp handle_update_result(error, _agent, state) do
     {state, error}
+  end
+
+  defp handle_fun(fun) do
+    try do
+      {:ok, fun.()}
+    catch
+      kind, error ->
+        {:error, {kind, error, __STACKTRACE__}}
+    end
+  end
+
+  defp handle_fun(fun, state) do
+    try do
+      {:ok, fun.(state)}
+    catch
+      kind, error ->
+        {:error, {kind, error, __STACKTRACE__}}
+    end
   end
 end
